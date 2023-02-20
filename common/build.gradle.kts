bbc75ca61ff2ce18494f713cb346f94dd6efd28b
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2023-02-20 14:42:41 +0300

    Use new versions and flags

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -54,7 +54,7 @@ kotlin {
                 api("androidx.core:core-ktx:1.9.0")
             }
         }
-        val androidTest by getting {
+        val androidUnitTest by getting {
             dependencies {
                 implementation("junit:junit:4.13.2")
             }
